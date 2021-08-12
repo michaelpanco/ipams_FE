62abe17b2a3da1521c6e3ffd88557fc67e4d4ad2
Michael Panco <micpanco@gmail.com>
2021-08-12 16:57:42 +0800

    customize MUI

---
+++ src/styles/muitheme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#007eca'
+        }
+    },
+    shadows: ["none"]
+})
+
+export default theme
