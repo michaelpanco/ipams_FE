2620d1d5282937d8169dd53eaded85be14d74370
Michael Panco <micpanco@gmail.com>
2021-08-13 02:36:36 +0800

    fix router dashboard redirection when refresh

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { AppRouter, AuthRouter } from './utils/AppRouter'
 
@@ -13,23 +13,34 @@ import Page404 from './pages/_commons/404';
 //Pages
 import Login from './pages/login'
 import Dashboard from './pages/dashboard'
+import IPAddress from './pages/ipaddress'
+import IPAddressDetails from './pages/ipaddress/details'
+import Activities from './pages/activities'
 
-const SystemAppRouter = ({ authenticated }) => (
+const SystemAppRouter = ({ authenticated, checked }) => (
     
     <Router>
-      
+       { checked &&
         <div className="container">
             <Switch>
-
                 <AppRouter exact path='/' layout={LayoutPlain} component={Login} authenticated={authenticated} />
-                <AuthRouter exact path='/dashboard' permission='access_dashboard' layout={LayoutDefaultInterior} component={Dashboard} authenticated={authenticated} />
+                <AuthRouter exact path='/dashboard' layout={LayoutDefaultInterior} component={Dashboard} authenticated={authenticated} />
+                <AuthRouter exact path='/ipaddress' layout={LayoutDefaultInterior} component={IPAddress} authenticated={authenticated} />
+                <AuthRouter exact path='/ipaddress/:id' layout={LayoutDefaultInterior} component={IPAddressDetails} authenticated={authenticated} />
+                <AuthRouter exact path='/activities' layout={LayoutDefaultInterior} component={Activities} authenticated={authenticated} />
                 <AppRouter layout={LayoutPlain} component={Page404} />
 
             </Switch>
         </div>
+        }
         
     </Router>
     
 );
 
-export default SystemAppRouter;
+const mapStateToProps = ({session}) => ({
+    authenticated: session.authenticated,
+    checked: session.checked,
+});
+
+export default connect(mapStateToProps)(SystemAppRouter);
