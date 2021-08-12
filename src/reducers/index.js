115261150a9a4ef4cfbd7c28329e8aeec7a2001d
Michael Panco <micpanco@gmail.com>
2021-08-13 02:43:48 +0800

    add common, ipaddress and activities reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux';
 import { sessionReducer } from 'redux-react-session';
 
 import account_reducer from './account'
+import ipaddress_reducer from './ipaddress'
+import common_reducer from './common'
 
 const allReducers = combineReducers({
 	session: sessionReducer,
 	account: account_reducer,
+	ipaddress: ipaddress_reducer,
+	common: common_reducer,
 });
 
 export default allReducers;
