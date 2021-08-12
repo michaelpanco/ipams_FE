9ca69a347800eebcd36d24e36552d44cf00894ae
Michael Panco <micpanco@gmail.com>
2021-08-12 16:56:07 +0800

    Integrate Redux and .env initializer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { Provider } from 'react-redux';
+import { sessionService } from 'redux-react-session';
+import { createStore, applyMiddleware } from 'redux';
+import isAuth from './utils/IsAuth'
+import thunk from 'redux-thunk';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Routers from './router';
+import rootReducer from "./reducers";
+import theme from './styles/muitheme'
+
+import './styles/global.css'
+
+// Support Environment variables
+require('dotenv').config()
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+);
+
+const validateSession = (session) => {
+    return isAuth();
+}
+
+const options = { refreshOnCheckAuth: false, redirectPath: '/', driver: 'COOKIES', validateSession};
+
+sessionService.initSessionService(store, options);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<MuiThemeProvider theme={theme}>
+		<Provider store={store}>
+			<React.StrictMode>
+				<Routers />
+			</React.StrictMode>
+		</Provider>
+
+	</MuiThemeProvider>,
+
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
